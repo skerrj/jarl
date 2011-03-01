00e2e69cab6850eab0e06fc04a0f3f4e0eaff8a4
scott.k.johnson <skerrj@gmail.com>
2011-03-01 00:27:15 -0800

    added tree widget

---
+++ jarlview/redis_tree_widget.py
@@ -0,0 +1,65 @@
+import os
+from Tkinter import *
+import imp
+
+import TreeWidget
+from TreeWidget import TreeItem
+from TreeWidget import TreeNode
+from TreeWidget import ScrolledCanvas
+
+import redis_tree
+
+class RedisItem(TreeItem):
+    def __init__(self, node_id):
+        self.node_id = node_id
+        self.node = redis_tree.redis_read(node_id)
+    
+    def GetText(self):
+        return self.node.data
+    
+    def IsEditable(self):
+        return True
+    
+    def SetText(self, data):
+        self.node.data = data
+        redis_tree.redis_write(self.node)
+    
+    def GetIconName(self):
+        if not self.IsExpandable():
+            return "python"
+    
+    def IsExpandable(self):
+        return self.node.childPtr != 0
+        
+    def GetSubList(self):
+        ns = self.getSubListHelp()
+        sublist = []
+        for n in ns:
+            item = RedisItem(n)
+            sublist.append(item)
+        return sublist
+    
+    def getSubListHelp(self):
+        sublist = []
+        sublist.append(self.node.childPtr)
+        r_node = redis_tree.redis_read(self.node.childPtr)
+        while ( r_node.siblingPtr != 0 ):
+            sublist.append(r_node.siblingPtr)
+            r_node = redis_tree.redis_read(r_node.siblingPtr)
+        return sublist
+
+def test(node_id):
+    root = Tk()
+    root.configure(bd=0, bg="yellow")
+    root.focus_set()
+    
+    sc = ScrolledCanvas(root, bg="white", highlightthickness=0, takefocus=1)
+    sc.frame.pack(expand=1, fill="both")
+    
+    item = RedisItem(node_id)
+    node = TreeNode(sc.canvas, None, item)
+    node.expand()
+
+if __name__ == '__main__':
+    test()
+
